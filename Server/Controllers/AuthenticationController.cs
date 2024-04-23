275285aaa585f908396ce6dd2fbc0a79fc59fa39
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-04-23 16:25:36 +0200

    Register controller for API testing

---
+++ Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using BaseLibrary.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServerLibrary.Repositories.Contracts;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController(IUserAccount accountInterface) : ControllerBase
+    {
+        [HttpPost("register")]
+
+        public async Task<IActionResult> CreateAsync(Register user)
+        {
+            if (user == null) return BadRequest("Model is empty");
+            var result = await accountInterface.CreateAsync(user);
+            return Ok(result);
+        }       
+
+    }
+}
