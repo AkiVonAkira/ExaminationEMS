a71643efc24557c6f2ea4539da31c233a141c1a9
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-04-24 20:00:14 +0200

    Update GetHttpClient.cs

--- ClientLibrary/Helpers/GetHttpClient.cs
+++ ClientLibrary/Helpers/GetHttpClient.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ClientLibrary.Helpers
 {
-    internal class GetHttpClient
+    public class GetHttpClient
     {
+        // kommer jobba med detta senare 
     }
 }
