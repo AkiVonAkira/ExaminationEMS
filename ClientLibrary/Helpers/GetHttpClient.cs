f019c7dbcd510694d3490f5d5c0784e1b3bdcd3a
Ilyas Kaya <113895247+AkiVonAkira@users.noreply.github.com>
2024-04-25 16:27:18 +0200

    fix lambda
    local data storage was incorrect and non functional
    and formatting to the other files

--- ClientLibrary/Helpers/GetHttpClient.cs
+++ ClientLibrary/Helpers/GetHttpClient.cs
@@ -1,10 +1,4 @@
 ﻿using BaseLibrary.DTOs;
-using Blazored.LocalStorage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClientLibrary.Helpers
 {
@@ -16,22 +10,22 @@ namespace ClientLibrary.Helpers
         {
             var client = httpClientFactory.CreateClient("SystemApiClient");
             var stringToken = await localStrorage.GetToken();
-           
-            var deserializeToken = Serializations.DeserializesJsonString<UserSession>(stringToken);   
 
-            if(deserializeToken == null)
+            var deserializeToken = Serializations.DeserializesJsonString<UserSession>(stringToken);
+
+            if (deserializeToken == null)
             {
                 return client;
             }
 
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", deserializeToken.Token);    
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", deserializeToken.Token);
         }
 
         public HttpClient GetPublicHttpClient()
         {
             var client = httpClientFactory.CreateClient("SystemApiClient");
             client.DefaultRequestHeaders.Remove(HeadKey);
-            return client; 
+            return client;
         }
     }
 }
--- ClientLibrary/Helpers/LocalDataStrorage.cs
+++ ClientLibrary/Helpers/LocalDataStrorage.cs
@@ -1,33 +1,28 @@
 ﻿using Blazored.LocalStorage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClientLibrary.Helpers
 {
-    public class LocalDataStrorage
+    public class LocalDataStrorage(ILocalStorageService localStorage)
     {
         // Namnet på nyckeln för att lagra autentiserings-token lokalt.
         private const string StorageKey = "authentication-token";
 
         // för att hämta autentiserings-token från lokal lagring.
-        public static async Task<string> GetToken(ILocalStorageService localStorage)
+        public async Task<string> GetToken()
         {
             // Hämta autentiserings-token som en sträng från lokal lagring.
             return await localStorage.GetItemAsStringAsync(StorageKey);
         }
 
         // för att ställa in autentiserings-token i lokal lagring.
-        public static async Task SetToken(ILocalStorageService localStorage, string token)
+        public async Task SetToken(string token)
         {
             // Spara autentiserings-token som en sträng i lokal lagring.
             await localStorage.SetItemAsStringAsync(StorageKey, token);
         }
 
         // för att ta bort autentiserings-token från lokal lagring.
-        public static async Task RemoveToken(ILocalStorageService localStorage)
+        public async Task RemoveToken()
         {
             // Ta bort autentiserings-token från lokal lagring.
             await localStorage.RemoveItemAsync(StorageKey);
