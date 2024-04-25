e2e89fa78caf5142abe090e1694b71d4653b2b51
Ilyas Kaya <113895247+AkiVonAkira@users.noreply.github.com>
2024-04-25 19:04:45 +0200

    add authentication to program cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ServerLibrary.Data;
 using ServerLibrary.Helpers;
 using ServerLibrary.Repositories.Contracts;
 using ServerLibrary.Repositories.Implementations;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
+var jwtSection = builder.Configuration.GetSection(nameof(JwtSection)).Get<JwtSection>();
+
 // Start Database Connection.
 //builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
@@ -21,7 +27,24 @@ builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
         throw new InvalidOperationException("Sorry no Database Connection was found!"));
 });
 
-builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = jwtSection!.Issuer,
+        ValidAudience = jwtSection!.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSection.Key!))
+    };
+});
+
 builder.Services.AddScoped<IUserAccount, UserAccountRepository>();
 
 builder.Services.AddCors(options =>
