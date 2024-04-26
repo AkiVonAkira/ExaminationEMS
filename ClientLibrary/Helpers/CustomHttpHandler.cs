a915826644ae81467fddcb5d3caf94cc85f84f2d
Caius <113854442+CaiusMatei@users.noreply.github.com>
2024-04-26 16:07:40 +0300

    added class to ClientLibrary

---
+++ ClientLibrary/Helpers/CustomHttpHandler.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace ClientLibrary.Helpers
+{
+    public class CustomHttpHandler : DelegatingHandler 
+    {
+        protected async override Task<HttpResponseMessage>SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            bool loginUrl = request.RequestUri!.AbsoluteUri.Contains("login");
+            bool registerUrl = request.RequestUri!.AbsoluteUri.Contains("register");
+            bool refreshTokenUrl = request.RequestUri!.AbsoluteUri.Contains("refresh-token");
+
+            if(loginUrl|| registerUrl || refreshTokenUrl)
+                return await base.SendAsync(request, cancellationToken);
+
+            var result = await base.SendAsync(request, cancellationToken);
+            if (result.StatusCode == HttpStatusCode.Unauthorized)
+            {
+
+
+            }
+            return result;
+
+        }
+    }
+}
