1a0cb7799f7d5750615c500eb2ea7d028e34c3e0
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-05-08 01:02:45 +0200

    Update CountryController.cs

--- Server/Controllers/CountryController.cs
+++ Server/Controllers/CountryController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BaseLibrary.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ServerLibrary.Repositories.Contracts;
 
 namespace Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CountryController : ControllerBase
+    public class CountryController(IGenericRepositoryInterface<Country> genericRepositoryInterface) : GenericController<Country>(genericRepositoryInterface)
     {
     }
 }
