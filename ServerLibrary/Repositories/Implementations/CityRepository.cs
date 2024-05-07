8abe5afe9082426108ce80a7ff69936384477221
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-05-08 00:36:06 +0200

    Added Implementation to City

---
+++ ServerLibrary/Repositories/Implementations/CityRepository.cs
@@ -0,0 +1,69 @@
+﻿using BaseLibrary.Models;
+using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class CityRepository(ApplicationDbContext applicationDbContext) : IGenericRepositoryInterface<City>
+    {
+        public async Task<GeneralResponse> DeleteById(int id)
+        {
+            var department = await applicationDbContext.Cities.FindAsync(id);
+            if (department is null)
+            {
+                return NotFound();
+            }
+
+            applicationDbContext.Cities.Remove(department);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<List<City>> GetAll() => await applicationDbContext.Cities.ToListAsync();
+
+        public async Task<City> GetById(int id)
+        {
+            return await applicationDbContext.Cities.FindAsync(id);
+        }
+
+        public async Task<GeneralResponse> Insert(City item)
+        {
+            if (!await CheckName(item.Name!))
+            {
+                return new GeneralResponse(false, "Sorry Department already exists");
+            }
+            applicationDbContext.Cities.Add(item);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<GeneralResponse> Update(City item)
+        {
+            var department = await applicationDbContext.Cities.FindAsync(item.Id);
+            if (department is null)
+            {
+                return NotFound();
+            }
+            department.Name = item.Name;
+            await Commit();
+            return Success();
+        }
+
+        private static GeneralResponse NotFound() => new(false, "Sorry Department not found");
+
+        private static GeneralResponse Success() => new(true, "Success, Process completed");
+
+        private async Task Commit()
+        {
+            await applicationDbContext.SaveChangesAsync();
+        }
+
+        private async Task<bool> CheckName(string name)
+        {
+            var nameItem = await applicationDbContext.Cities.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
+            return nameItem is null;
+        }
+    }
+}
