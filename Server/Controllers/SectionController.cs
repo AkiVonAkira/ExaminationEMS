e4c6e3d4cd5ecc9194ac81cb8eb549cb5f7b7105
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-05-08 01:00:39 +0200

    Create SectionController.cs

---
+++ Server/Controllers/SectionController.cs
@@ -0,0 +1,13 @@
+﻿using BaseLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServerLibrary.Repositories.Contracts;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SectionController(IGenericRepositoryInterface<Section>genericRepositoryInterface) : GenericController<Section>(genericRepositoryInterface)
+    {
+    }
+}
