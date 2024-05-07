2d361d110767e5fa362b67b09cbc4278b2babac4
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-05-08 01:05:44 +0200

    Update CityController.cs

--- Server/Controllers/CityController.cs
+++ Server/Controllers/CityController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BaseLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
+using ServerLibrary.Repositories.Contracts;
 
 namespace Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CityController : ControllerBase
+    public class CityController(IGenericRepositoryInterface<City> genericRepositoryInterface)
+        : GenericController<City>(genericRepositoryInterface)
     {
     }
 }
