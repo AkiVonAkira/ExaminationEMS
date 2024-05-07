8167f4fb14a219d070f110ee8c8aca2e007832b9
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-05-07 23:54:52 +0200

    Create DepartmentRepository.cs

---
+++ ServerLibrary/Repositories/Implementations/DepartmentRepository.cs
@@ -0,0 +1,40 @@
+﻿using BaseLibrary.Models;
+using BaseLibrary.Responses;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class DepartmentRepository(ApplicationDbContext applicationDbContext) : IGenericRepositoryInterface<Department>
+    {
+        public Task<GeneralResponse> DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Department>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Department> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse> Insert(Department item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse> Update(Department item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
