b5929e74884c269866df4b91d0cdc691263ae07a
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-05-08 01:08:01 +0200

    Update StateController.cs

--- Server/Controllers/StateController.cs
+++ Server/Controllers/StateController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BaseLibrary.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ServerLibrary.Repositories.Contracts;
 
 namespace Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StateController : ControllerBase
+    public class StateController(IGenericRepositoryInterface<State>genericRepositoryInterface) : GenericController<State>(genericRepositoryInterface)
     {
     }
 }
