1444e9d4e6559c84dc4d2175cbae7ae810e6436f
chasacademy-Muse-Yusuf <114033186+chasacademy-Muse-Yusuf@users.noreply.github.com>
2024-05-13 01:24:12 +0200

    Addded Manageuser page, dialog & Implement user management methods in UserAccountService
    UserDialog is some error in the file, Check it out

--- ClientLibrary/Services/Implementations/UserAccountService.cs
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -1,4 +1,5 @@
 ﻿using BaseLibrary.DTOs;
+using BaseLibrary.Models;
 using BaseLibrary.Responses;
 using ClientLibrary.Helpers;
 using ClientLibrary.Services.Contracts;
@@ -35,5 +36,42 @@ namespace ClientLibrary.Services.Implementations
             //för att uppdatera autentiserings-token
             throw new NotImplementedException();
         }
+
+        public async Task<List<ManageUser>> GetUsers()
+        {
+            //för att hämta alla användare
+            var httpClient = getHttpClient.GetPrivateHttpClient();
+            var result = await httpClient.GetFromJsonAsync<List<ManageUser>>($"{AuthUrl}/users");
+            return result!;
+        }
+
+        public async Task<GeneralResponse> UpdateUser(ManageUser user)
+        {
+            //för att uppdatera en användare
+            var httpClient = getHttpClient.GetPublicHttpClient();
+            var result = await httpClient.PutAsJsonAsync($"{AuthUrl}/update-user", user);
+            if (!result.IsSuccessStatusCode) return new GeneralResponse(false, " Error occured");
+
+            return await result.Content.ReadFromJsonAsync<GeneralResponse>()!;
+        }
+
+        public async Task<List<SystemRole>> GetRoles()
+        {
+            //för att hämta alla roller
+            var httpClient = getHttpClient.GetPrivateHttpClient();
+            var result = await httpClient.GetFromJsonAsync<List<SystemRole>>($"{AuthUrl}/roles");
+            return result!;
+        }
+
+        public async Task<GeneralResponse> DeleteUser(int id)
+        {
+            //för att ta bort en användare
+            var httpClient = getHttpClient.GetPrivateHttpClient();
+            var result = await httpClient.DeleteAsync($"{AuthUrl}/delete-user/{id}");
+            if (!result.IsSuccessStatusCode) return new GeneralResponse(false, " Error occured");
+
+            return await result.Content.ReadFromJsonAsync<GeneralResponse>()!;
+        }
+
     }
 }
